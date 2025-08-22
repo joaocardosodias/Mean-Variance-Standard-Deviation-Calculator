36b7af213116504a09a58a0e71053a9e30ac9013
João Cardoso Dias <joao.dias@sou.inteli.edu.br>
2025-08-21 21:24:57 -0300

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,47 @@
 import numpy as np
 
-def calculate(list):
-
-
-
-
+def calculate(lista):
+    
+    if len(lista) != 9:
+        raise ValueError("the list must contain exactly 9 numbers")
+    
+    # Converte a lista em matriz 3x3
+    matriz = np.array(lista).reshape(3, 3)
+    
+   
+    calculations = {
+        'mean': [
+            matriz.mean(axis=0).tolist(),  
+            matriz.mean(axis=1).tolist(), 
+            matriz.mean().tolist()         
+        ],
+        'variance': [
+            matriz.var(axis=0).tolist(),
+            matriz.var(axis=1).tolist(),
+            matriz.var().tolist()
+        ],
+        'standard deviation': [
+            matriz.std(axis=0).tolist(),
+            matriz.std(axis=1).tolist(),
+            matriz.std().tolist()
+        ],
+        'max': [
+            matriz.max(axis=0).tolist(),
+            matriz.max(axis=1).tolist(),
+            matriz.max().tolist()
+        ],
+        'min': [
+            matriz.min(axis=0).tolist(),
+            matriz.min(axis=1).tolist(),
+            matriz.min().tolist()
+        ],
+        'sum': [
+            matriz.sum(axis=0).tolist(),
+            matriz.sum(axis=1).tolist(),
+            matriz.sum().tolist()
+        ]
+    }
+    
     return calculations
+
+print(calculate([0,1,2,3,4,5,6,7,8]))
